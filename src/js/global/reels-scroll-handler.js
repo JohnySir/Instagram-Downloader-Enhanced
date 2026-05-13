3adc11425975974ec58b98646bded799713f8d28
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2026-05-13 08:18:00 +0700

    update logic to capture post id on reels page

--- src/js/global/reels-scroll-handler.js
+++ src/js/global/reels-scroll-handler.js
@@ -3,9 +3,9 @@ navigation.addEventListener('navigate', e => {
     const url = new URL(e.destination.url);
     const match = url.pathname.match(/\/(reels)\/([A-Za-z0-9_-]*)(\/?)/);
     if (match) {
-        const reels = document.querySelectorAll('main>div>div:nth-child(odd)');
+        const reels = document.querySelectorAll('main>div>div');
         reels.forEach(element => {
-            const queryReference = getValueByKey(element, 'queryReference');
+            const queryReference = getValueByKey(element, 'PolarisClipsViewer_media_identifier');
             if (queryReference?.pk && queryReference?.code && queryReference?.code === match[2]) {
                 window.dispatchEvent(new CustomEvent('postView', {
                     detail: {
